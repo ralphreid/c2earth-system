c8f2bfcb9881b66d91abd93bce43d61c737f2d69
Ralph Reid <beresfordjunior@me.com>
2014-04-26 15:59:40 +0100

    completed generate_dropbox_download_url method

--- lib/dropbox_content.rb
+++ lib/dropbox_content.rb
@@ -5,60 +5,61 @@ require "awesome_print"
 
 class DropboxContent
 # constructor method
-def initialize(dropbox_url, header_present)
-  @dropbox_url, @header_present = dropbox_url, header_present
-end
+  def initialize(dropbox_url, header_present)
+    @dropbox_url, @header_present = dropbox_url, header_present
+  end
 
-# setter methods
-def set_dropbox_url=(value)
-  @dropbox_url = value
-end
+  # setter methods
+  def set_dropbox_url=(value)
+    @dropbox_url = value
+  end
 
-def set_header_present=(value)
-  @header_present = value
-end
+  def set_header_present=(value)
+    @header_present = value
+  end
 
-# accessor methods
-def get_dropbox_url
-  @dropbox_url
-end
+  # accessor methods
+  def get_dropbox_url
+    @dropbox_url
+  end
 
-def get_header_present
-  @header_present
-end
+  def get_header_present
+    @header_present
+  end
 
-# instance methods
+  # instance methods
 
-def generate_dropbox_download_url
-  # strip https://www.dropbox.com from dropbox url
-  dropbox_path = @dropbox_url.at(0..10)
-  ap dropbox_path
-  # generate download url
-  # @dropbox_download_url = "https://dl.dropboxusercontent.com/#{dropbox_path}"
-  # puts @dropbox_download_url
-end
+  def generate_dropbox_download_url
+    # strip https://www.dropbox.com from dropbox url
+    dropbox_path = @dropbox_url.at(24..-1)
+    dropbox_download_ending = '?dl=1'
+    # generate download url
+    @dropbox_download_url = "https://dl.dropboxusercontent.com/#{dropbox_path}#{dropbox_download_ending}"
+    ap @dropbox_download_url
+  end
 
-def get_array
-  puts 'get_array'
-end
+  def get_array
+    puts 'get_array'
+  end
 
-def get_array_of_arrays
-  puts 'get_array_of_arrays'
-end
+  def get_array_of_arrays
+    puts 'get_array_of_arrays'
+  end
 
-def find_headers
-  puts 'find_headers'
-end
+  def find_headers
+    puts 'find_headers'
+  end
 
-def remove_header
-  puts 'remove_header'
-end
+  def remove_header
+    puts 'remove_header'
+  end
 
 
-# Future Feature
-def check_for_headers
-  
-end
+  # Future Feature
+  def check_for_headers
+    
+  end
+
 end
 
 
