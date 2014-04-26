981e23c7a83bb80acd7987a8d16bedf1ad8f4af0
Ralph Reid <beresfordjunior@me.com>
2014-04-27 00:25:05 +0100

    removed contitionals on users, need better way

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,14 @@ require 'dropbox_content'
 # Initial seed file to use with Devise User Model
 
 # Temporary admin account
+
 u = User.new(
     username: "admin",
     email: "admin@example.com",
     password: "1234",
     password_confirmation: "1234",
     admin: true
-) if User.count == 0
+)
 u.skip_confirmation!
 u.save!
 
@@ -20,7 +21,7 @@ u.save!
       email: "user#{i}@example.com",
       password: "1234",
       password_confirmation: "1234"
-  ) if User.count == 0
+  )
   u.skip_confirmation!
   u.save!
 
