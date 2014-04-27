6eb00ca482a0bc21ee72a0645dc307dd3e85c5d9
Ralph Reid <beresfordjunior@me.com>
2014-04-27 11:44:46 +0100

    completed client_types seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,55 @@ require 'dropbox_content'
 # Generated with RailsBricks
 # Initial seed file to use with Devise User Model
 
-# Temporary admin account
-
-u = User.new(
+# Create new user accounts only if non-exist
+if User.count == 0
+  # Temporary admin account
+  u = User.new(
     username: "admin",
     email: "admin@example.com",
     password: "1234",
     password_confirmation: "1234",
     admin: true
-)
-u.skip_confirmation!
-u.save!
-
-# Test user accounts
-(1..5).each do |i|
-  u = User.new(
-      username: "user#{i}",
-      email: "user#{i}@example.com",
-      password: "1234",
-      password_confirmation: "1234"
   )
   u.skip_confirmation!
   u.save!
 
-  puts "#{i} test users created..." if (i % 5 == 0)
+  # Test user accounts
+  (1..5).each do |i|
+    u = User.new(
+        username: "user#{i}",
+        email: "user#{i}@example.com",
+        password: "1234",
+        password_confirmation: "1234"
+    )
+    u.skip_confirmation!
+    u.save!
 
+    puts "#{i} test users created..." if (i % 5 == 0)
+  end
 end
+
+
+# client_types records
+db_url = "https://www.dropbox.com/s/ou0e9a0bat0wl1f/ProjectsbyClientType.txt"
+pms = DropboxContentSeed.new(db_url, true)
+client_types = pms.get_arr_of_arrs
+client_types.each do |row|
+  ClientType.create(client_type: row[0])
+end
+puts "#{ClientType.count} Client Types created"
+
+
+# companies records
+
+
+# faults records
+
+
+# phases records
+
+
+
 
 # project_managers records
 db_url = "https://www.dropbox.com/sh/sl727ql5hyffqn5/FhptN8NQu3/ProjectManagers.txt"
@@ -40,5 +63,21 @@ project_managers.each do |row|
     manager_lastname: 'mee too, mon',
     name: row[1])
 end
-
 puts "#{ProjectManager.count} Project Managers created"
+
+
+# projects records
+
+
+# sites records
+
+
+# stakeholder_types records
+
+
+# stakeholders
+
+
+# structure_types records
+
+
