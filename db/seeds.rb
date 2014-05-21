38323ddf92ffd1fd27cd5f376a251b512919e777
Ralph Reid <beresfordjunior@me.com>
2014-05-21 23:29:29 +0100

    turned ON company seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -114,11 +114,12 @@ pms = ArrayContent.new(db_url_companies, true, 'local')
 companies = pms.get_arr_of_arrs
 companies.each do |row|
   c = Company.create!( name: row[1] )
-  company_to_add = row[0]
-  company_to_add.capitalize
-  t = CompanyType.find_by name: company_to_add
-  c.company_types.push t
-  c.save!
+  type_to_add = row[0]
+  type_to_add.capitalize
+  t = CompanyType.find_by name: type_to_add
+  unless t.nil?
+    c.company_types.push t
+  end
 end
 puts "#{Company.count} Companies created from #{pms.get_type}"
 
