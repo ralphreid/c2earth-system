4c0c1e3c55d9114ac7bea334d2649d95d977954c
ralphreid <beresfordjunior@me.com>
2014-07-09 19:23:04 +0100

    compoleted client rake

--- db/seeds_old.rb
+++ db/seeds_old.rb
@@ -46,21 +46,21 @@ phase_switch = 'off'
 #   end
 # end
 
-# client_types records
-case client_type_switch
-when 'on'
-  ClientType.destroy_all if ClientType.exists?
-  db_url_client_types = "client_types.txt"
-  ClientType.create!(name: 'TBC')
-  pms = ArrayContent.new(db_url_client_types, true, 'local')
-  client_types = pms.get_arr_of_arrs
-  client_types.each do |row|
-    client_type_to_add = row[0]
-    client_type_to_add.capitalize
-    ClientType.create!(name: client_type_to_add)
-  end
-  puts "#{ClientType.count} Client Types created from #{pms.get_type}"
-end
+# # client_types records
+# case client_type_switch
+# when 'on'
+#   ClientType.destroy_all if ClientType.exists?
+#   db_url_client_types = "client_types.txt"
+#   ClientType.create!(name: 'TBC')
+#   pms = ArrayContent.new(db_url_client_types, true, 'local')
+#   client_types = pms.get_arr_of_arrs
+#   client_types.each do |row|
+#     client_type_to_add = row[0]
+#     client_type_to_add.capitalize
+#     ClientType.create!(name: client_type_to_add)
+#   end
+#   puts "#{ClientType.count} Client Types created from #{pms.get_type}"
+# end
 
 # client records
 case client_switch
--- lib/tasks/legacy_data.rake
+++ lib/tasks/legacy_data.rake
@@ -2,6 +2,8 @@ namespace :legacy_data do
   desc "Load all legacy data"
   task load: :environment do
     # Rake::Task["user:create_test_users"].invoke
-    Rake::Task["client_type:create_client_types"].invoke
+    # Rake::Task["client_type:create_client_types"].invoke
+    Rake::Task["client:create_clients"].invoke
+
   end
 end
